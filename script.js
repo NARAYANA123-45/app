6168b06e0676f00139fc3a47e8aaebdc7fbd4267
NARAYANA123-45 <narayan.bujji@gmail.com>
2020-10-17 21:57:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let Ul = document.getElementById('lists');
+  let Btn2= document.getElementById("btn");
+      Btn2.addEventListener("click",function Addelement(){
+  let Inputvalue= document.getElementById("input").value;
+  let Text=document.createTextNode(Inputvalue);
+     
+       let Li =document.createElement("li");
+      let Checkbox = document.createElement("input");
+        Checkbox.type = 'checkbox';
+     Checkbox.setAttribute('id','check');
+    let Label = document.createElement('label');
+    Ul.appendChild(Label);
+    Li.appendChild(Checkbox);
+	Li.appendChild(Label);	
+   Label.appendChild(Text);
+   Ul.appendChild(Li);
+ 
+  })
+                                            
